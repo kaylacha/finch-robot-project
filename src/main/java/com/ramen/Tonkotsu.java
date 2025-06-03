dc3e4c5b5ab1fc4483183dc45d4bd49e581327a1
emeryahn <eahn0003@mymail.lausd.net>
2025-06-03 14:02:24 -0700

    fixed Tonkotsu.java

---
+++ src/main/java/com/ramen/Tonkotsu.java
@@ -0,0 +1,15 @@
+package com.emeryahn;
+
+import com.birdbrain.Finch;
+
+public class Tonkotsu {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+        bird.stop();
+        bird.disconnect();
+        
+
+
+    }
+}
